f5701c7fb521ee2fd868e2c1ab7d8767fb90587a
Mtiori <matheusioriribeiro@gmail.com>
2020-04-08 21:32:24 -0300

    adicionando sequelize

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect:    'mysql',
+    host:       'localhost',
+    username:   'root',
+    passaword:  '123456',
+    database:   'petshop',
+    define:{
+        timestamps: true,
+        underscored: true,
+    },
+};
